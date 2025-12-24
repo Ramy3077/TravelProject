4debc7dac2f943c470aff9310e8633419d89fb96
Ramy3077 <114334653+Ramy3077@users.noreply.github.com>
2025-12-25 00:26:29 +0100

    feat: Initialize database integration with JPA, PostgreSQL, and H2 for testing.

---
+++ api/src/test/java/com/travel/tripcost/TripCostApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.tripcost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripCostApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

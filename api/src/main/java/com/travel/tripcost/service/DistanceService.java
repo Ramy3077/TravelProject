0bb9baab34096412f9eab49ec29a8387e35f8daa
Ramy3077 <114334653+Ramy3077@users.noreply.github.com>
2025-12-28 04:31:38 +0100

    feat: add Distance Service

---
+++ api/src/main/java/com/travel/tripcost/service/DistanceService.java
@@ -0,0 +1,18 @@
+package com.travel.tripcost.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DistanceService {
+    public double calculateDistanceKm(double lat1, double lon1, double lat2, double lon2) {
+        double R = 6371; // Earth's radius in kilometers
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                        * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c; // Distance in kilometers
+    }
+
+}
